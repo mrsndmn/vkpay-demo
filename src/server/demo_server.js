ecfbe5cbb708dda3994f96f8ab9d3da25ab0d6e4
Dmitry Tarasov <d.tarasov@corp.mail.ru>
2018-09-18 09:45:05 +0300

    decorations and cant refunds in progress

--- src/server/demo_server.js
+++ src/server/demo_server.js
@@ -1,29 +1,41 @@
 let express = require('express');
 let app = express();
 
-require('dotenv').load();
-
 let sha1 = require('sha1');
 let md5 = require('md5');
+var base64 = require('base-64'); // todo check if it need to utf8encode before
+let openssl = require('openssl-verify');
+
 let fs = require('fs');
+let axios = require('axios');
+const querystring = require('querystring');
 
-var openssl = require('openssl-verify');
+////
+//// Prepearing enviroment
+////
 
+require('dotenv').load();
 const MERCH_ID = process.env.MERCH_ID;
 const MERCH_PRIVATE_KEY = process.env.MERCH_PRIVATE_KEY;
 const APP_SECRET_KET    = process.env.APP_SECRET_KET;
 
-if(!MERCH_ID || !MERCH_PRIVATE_KEY || !APP_SECRET_KET){
+let last_order_id = process.env.LAST_ORDER_ID || 1;
+
+if(!MERCH_ID || !MERCH_PRIVATE_KEY || !APP_SECRET_KET) {
   console.log("One of theese MERCH_ID MERCH_PRIVATE_KEY APP_SECRET_KET enviroment variables is not defined. Please fill the .env file or see README.md");
-  process.exit(1)
+  process.exit(1);
 }
 
-let last_order_id = process.env.LAST_ORDER_ID || 1;
 console.log("MERCH_ID " + MERCH_ID +
-            "\nMERCH_PRIVATE_KEY " + MERCH_PRIVATE_KEY +
-            "\nAPP_SECRET_KET " + APP_SECRET_KET +
-            "\nlast_order_id=" + last_order_id)
+  "\nMERCH_PRIVATE_KEY " + MERCH_PRIVATE_KEY +
+  "\nAPP_SECRET_KET " + APP_SECRET_KET +
+  "\nlast_order_id=" + last_order_id);
 
+
+
+////
+//// Helper function
+////
 
 // each transaction must have its unique order_id
 function dummySaveLastOrderID(last_order_id) {
@@ -36,19 +48,27 @@ function dummySaveLastOrderID(last_order_id) {
   fs.writeFileSync('.env', newValue, 'utf-8');
 }
 
+
+////
+//// Server controllers
+////
+
+////// Getting apps params
+// todo fetch any usefull data from request
+
 app.get('/app_params', function (req, res) {
   let amount = 1;
 
   merch_data = {
     "amount": amount,
-    "order_id": last_order_id,
+    "order_id": ++last_order_id,
     "currency": "RUB",
-    "ts": + new Date(),
+    "ts": (Date.now() / 1000 | 0),
   };
 
-  dummySaveLastOrderID(++last_order_id);
+  dummySaveLastOrderID(last_order_id);
 
-  merch_data_base64 = Buffer.from(JSON.stringify(merch_data)).toString('base64')
+  merch_data_base64 = base64.encode(JSON.stringify(merch_data))
 
   let data = {
       "order_id": merch_data.order_id,
@@ -76,13 +96,15 @@ app.get('/app_params', function (req, res) {
   res.json(pay_window_params); // responsing with json
 });
 
+////// url_for_payment_status_notifications :)
+
 app.post('/url_for_payment_status_notifications', (req, res) => {
   // var certificate = fs.readFileSync('certificate.pem', "utf8");
   console.log("in url_for_payments_status_notifications:\n", req);
   req = req;
 
   let data = {};
-  let sign = sha1( Buffer.from(JSON.stringify(data) + MERCH_PRIVATE_KEY ).toString('base64') )
+  let sign = sha1( base64.encode(JSON.stringify(data) + MERCH_PRIVATE_KEY ) )
 
   res.json({"data": data, "signature": sign});
 
@@ -92,50 +114,51 @@ app.post('/url_for_payment_status_notifications', (req, res) => {
 
 })
 
-//
-// REFUNDS
-//
+
+////
+//// REFUNDS
+////
+
+const DMR_API_URL = 'https://api-spare.money.mail.ru';
+const DMR_REFUND_URL = '/money/2-02/transaction/refund'; // its important thet theere is no slash at the end
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+
+app.post("/refund", (req, res) => {
+  console.log("in refund");
+
+  let data = {
+    body: {
+      transaction_id: req.query["txn_id"],
+      reason: req.query["reason"]
+    },
+    header: {
+      ts: ((+new Date()) / 1000 | 0),
+      client_id: MERCH_ID
+    }
+  };
+
+  console.log("data:", data);
+
+  let base64_data = base64.encode(JSON.stringify(data));
+  let sign = sha1( DMR_REFUND_URL + base64_data + MERCH_PRIVATE_KEY );
+  console.log("data:", base64_data);
+  console.log("sign:", sign);
+  console.log("body:", querystring.stringify({ data: base64_data, signature: sign }));
+  axios.post(DMR_API_URL + DMR_REFUND_URL, querystring.stringify({ data: base64_data, signature: sign }))
+    .then(function(response) {
+        console.log(response.data);
+        console.log(response.status);
+        console.log(response.statusText);
+        console.log(response.headers);
+        console.log(response.config);
+    }).catch((error) => {
+      console.log('Promise error', error.message);
+    });;
+
+  res.json({ status: "ok" });
 
 
-// const DMR_API_URL = 'https://api-spare.money.mail.ru';
-// const DMR_REFUND_URL = '/money/2-02/transaction/refund';
-// axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-
-// app.post("/refund", (req, res) => {
-//   console.log("in refund");
-
-//   let data = {
-//     body: {
-//       transaction_id: req.query["txn_id"],
-//       reason: req.query["reason"]
-//     },
-//     header: {
-//       ts: + new Date(),
-//       client_id: MERCH_ID
-//     }
-//   };
-
-//   console.log("data:", data);
-
-//   let base64_data = Buffer.from(JSON.stringify(data)).toString('base64');
-//   let sign = sha1( DMR_REFUND_URL + base64_data + MERCH_PRIVATE_KEY )
-//   const querystring = require('querystring');
-//   axios.post(DMR_API_URL + DMR_REFUND_URL, querystring.stringify({ data: base64_data, signature: sign }))
-//     .then(function(response) {
-//         console.log(response.data);
-//         console.log(response.status);
-//         console.log(response.statusText);
-//         console.log(response.headers);
-//         console.log(response.config);
-//     }).catch((error) => {
-//       console.log('Promise error', error);
-//       done();
-//     });;
-
-//   res.json({ status: "ok" });
-
-
-// })
+})
 
 app.listen(process.env.VKPAY_DEMO_BACKEND_PORT, function () {
   console.log('Example app listening on port ' + process.env.VKPAY_DEMO_BACKEND_PORT);
