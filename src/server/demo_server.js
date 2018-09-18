824b5b4913ead45e5649ff95598c82afba793569
Dmitry Tarasov <d.tarasov@corp.mail.ru>
2018-09-18 15:55:26 +0300

    format document

--- src/server/demo_server.js
+++ src/server/demo_server.js
@@ -22,11 +22,11 @@ const querystring = require('querystring');
 require('dotenv').load();
 const MERCH_ID = process.env.MERCH_ID;
 const MERCH_PRIVATE_KEY = process.env.MERCH_PRIVATE_KEY;
-const APP_SECRET_KET    = process.env.APP_SECRET_KET;
+const APP_SECRET_KET = process.env.APP_SECRET_KET;
 
 let last_order_id = process.env.LAST_ORDER_ID || 1;
 
-if(!MERCH_ID || !MERCH_PRIVATE_KEY || !APP_SECRET_KET) {
+if (!MERCH_ID || !MERCH_PRIVATE_KEY || !APP_SECRET_KET) {
   console.log("One of theese MERCH_ID MERCH_PRIVATE_KEY APP_SECRET_KET enviroment variables is not defined. Please fill the .env file or see README.md");
   process.exit(1);
 }
@@ -44,7 +44,7 @@ console.log("MERCH_ID " + MERCH_ID +
 
 // each transaction must have its unique order_id
 function dummySaveLastOrderID(last_order_id) {
-  let new_order_id_str = 'LAST_ORDER_ID='+last_order_id;
+  let new_order_id_str = 'LAST_ORDER_ID=' + last_order_id;
   let data = fs.readFileSync('.env', 'utf-8');
   let newValue = data.replace(/^(LAST_ORDER_ID=(\d+)?)/gim, new_order_id_str);
   if (newValue === data)
@@ -65,10 +65,10 @@ app.get('/app_params', function (req, res) {
   let amount = 1;
 
   merch_data = {
-    "amount": amount,
-    "order_id": ++last_order_id,
-    "currency": "RUB",
-    "ts": (Date.now() / 1000 | 0),
+    amount: amount,
+    order_id: ++last_order_id,
+    currency: "RUB",
+    ts: (Date.now() / 1000 | 0),
   };
 
   dummySaveLastOrderID(last_order_id);
@@ -76,25 +76,25 @@ app.get('/app_params', function (req, res) {
   merch_data_base64 = base64.encode(JSON.stringify(merch_data))
 
   let data = {
-      "order_id": merch_data.order_id,
-      "ts": merch_data.ts,
-      "currency": "RUB",
-      "merchant_data": merch_data_base64,
-      "merchant_sign": sha1( merch_data_base64 + MERCH_PRIVATE_KEY ),
-      "my_own_key": 123123123123 // you can put here any data you want
+    order_id: merch_data.order_id,
+    ts: merch_data.ts,
+    currency: "RUB",
+    merchant_data: merch_data_base64,
+    merchant_sign: sha1(merch_data_base64 + MERCH_PRIVATE_KEY),
+    my_own_key: 123123123123 // you can put here any data you want
   };
 
   let pay_window_params = {
-    "amount": amount,
-    "data": JSON.stringify(data),
-    "description": "Оплата заказа",
-    "action": "pay-to-service",
-    "merchant_id": MERCH_ID,
+    amount: amount,
+    data: JSON.stringify(data),
+    description: "Оплата заказа",
+    action: "pay-to-service",
+    merchant_id: MERCH_ID,
   }
 
   let params = ""
-  Object.keys(pay_window_params).sort((a,b) => a > b).forEach(function(key){ if (key != "action") params += key + "=" + pay_window_params[key] })
-  console.log("params=\n", params )
+  Object.keys(pay_window_params).sort((a, b) => a > b).forEach(function (key) { if (key != "action") params += key + "=" + pay_window_params[key] })
+  console.log("params=\n", params)
   pay_window_params.sign = md5(params + APP_SECRET_KET)
 
   console.log("response:" + JSON.stringify(pay_window_params));
@@ -109,7 +109,6 @@ app.post('/url_for_payment_status_notifications', (req, res) => {
   let req_data = JOSN.parse(base64.decode(req.body.data));
   console.log(" req_data:", req_data);
 
-
   let data = {
     body: {
       transaction_id: req_data.transaction_id,
@@ -121,10 +120,10 @@ app.post('/url_for_payment_status_notifications', (req, res) => {
       client_id: MERCH_ID
     }
   };
-  let sign = sha1( base64.encode(JSON.stringify(data) + MERCH_PRIVATE_KEY ) )
+  let sign = sha1(base64.encode(JSON.stringify(data) + MERCH_PRIVATE_KEY))
 
   // responsing wit that json
-  res.json({"data": data, "signature": sign, version: "2-02"});
+  res.json({ data: data, signature: sign, version: "2-02" });
 
   // openssl.verifyCertificate(certificate, 'certs', function(result) {
   //     console.log(result);
@@ -158,17 +157,17 @@ app.post("/refund", (req, res) => {
   console.log("data:", data);
 
   let base64_data = base64.encode(JSON.stringify(data));
-  let sign = sha1( DMR_REFUND_URL + base64_data + MERCH_PRIVATE_KEY );
+  let sign = sha1(DMR_REFUND_URL + base64_data + MERCH_PRIVATE_KEY);
   console.log("data:", base64_data);
   console.log("sign:", sign);
   console.log("body:", querystring.stringify({ data: base64_data, signature: sign }));
   axios.post(DMR_API_URL + DMR_REFUND_URL, querystring.stringify({ data: base64_data, signature: sign }))
-    .then(function(response) {
-        console.log(response.data);
-        console.log(response.status);
-        console.log(response.statusText);
-        console.log(response.headers);
-        console.log(response.config);
+    .then(function (response) {
+      console.log(response.data);
+      console.log(response.status);
+      console.log(response.statusText);
+      console.log(response.headers);
+      console.log(response.config);
     }).catch((error) => {
       console.log('Promise error', error.message);
     });;
